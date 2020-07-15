1c46f0f9a09b8a0f27cdd6f195a4b2fda67f6425
yaxy2 <58487644+yaxy2@users.noreply.github.com>
2020-07-15 22:32:27 +0200

    implement some timestamp functions

--- mqttClient/create_json_sensordata.py
+++ mqttClient/create_json_sensordata.py
@@ -1,4 +1,7 @@
 import json
+import time
+
+
 
 # Dummy data
 
@@ -23,16 +26,18 @@ gyro_z = 83
 
 # Dummy timestamp data
 
-ts_lidar = 2345634534566
-ts_humidity = 533452345254
-ts_steering_angle = 3234523452345
-ts_temperature = 545463563456
-ts_speed = 54345345345
-ts_altimeter = 1234523542345
+ts = time.time()*1000
 
-ts_acc = 4332463635
-ts_mag = 2345634563465
-ts_gyro = 952345234523563
+# ts_lidar = 2345634534566
+# ts_humidity = 533452345254
+# ts_steering_angle = 3234523452345
+# ts_temperature = 545463563456
+# ts_speed = 54345345345
+# ts_altimeter = 1234523542345
+
+# ts_acc = 4332463635
+# ts_mag = 2345634563465
+# ts_gyro = 952345234523563
 
 
 # Define JSON and pass values
@@ -43,18 +48,18 @@ public class JSON_dumper:
 
 sensor_data =   {
                     "SensorValue1": [
-                        {"name":"LIDAR", "timestamp":ts_lidar, "value":sensorData.lidar},
-                        {"name":"Humidity", "timestamp":ts_humidity, "value":sensorData.humidity},
-                        {"name":"SteeringAngle","timestamp":ts_steering_angle, "value":sensorData.steering_angle},
-                        {"name":"Temperature", "timestamp":ts_temperature, "value":sensorData.temperature},
-                        {"name":"Speed", "timestamp":ts_speed, "value":sensorData.speed},
-                        {"name":"Altimeter", "timestamp":ts_altimeter, "value":sensorData.altimeter}
+                        {"name":"LIDAR", "timestamp":ts, "value":sensorData.lidar},
+                        {"name":"Humidity", "timestamp":ts, "value":sensorData.humidity},
+                        {"name":"SteeringAngle","timestamp":ts, "value":sensorData.steering_angle},
+                        {"name":"Temperature", "timestamp":ts, "value":sensorData.temperature},
+                        {"name":"Speed", "timestamp":ts, "value":sensorData.speed},
+                        {"name":"Altimeter", "timestamp":ts, "value":sensorData.altimeter}
                     ],
 
                     "SensorValue3": [
-                        {"name":"Acceleration", "timestamp":ts_acc, "valueX":sensorData.acc.acc_x, "valueY":sensorData.acc.acc_y, "valueZ":sensorData.acc.acc_z},
-                        {"name":"Magnetometer", "timestamp":ts_mag, "valueX":sensorData.mag.mag_x, "valueY":sensorData.mag.mag_y, "valueZ":sensorData.mag.mag_z},
-                        {"name":"Gyro", "timestamp":ts_gyro, "valueX":sensorData.gyro.gyro_x, "valueY":gyro_y, "valueZ":gyro_z}
+                        {"name":"Acceleration", "timestamp":ts, "valueX":sensorData.acc.acc_x, "valueY":sensorData.acc.acc_y, "valueZ":sensorData.acc.acc_z},
+                        {"name":"Magnetometer", "timestamp":ts, "valueX":sensorData.mag.mag_x, "valueY":sensorData.mag.mag_y, "valueZ":sensorData.mag.mag_z},
+                        {"name":"Gyro", "timestamp":ts, "valueX":sensorData.gyro.gyro_x, "valueY":gyro_y, "valueZ":gyro_z}
                     ]
                 }
 
