e6ad777d1050f96f201319faaa7f28e82e6b366e
yaxy2 <58487644+yaxy2@users.noreply.github.com>
2020-07-15 22:42:15 +0200

    constructor implemented

--- mqttClient/dataStruct.py
+++ mqttClient/dataStruct.py
@@ -1,2 +1,27 @@
 public class dataStruct:
   
+  lidar = 0
+  humidity = 0
+  steering_angle = 0
+  temperature = 0
+  speed = 0
+  altimeter = 0
+  acc = [0, 0, 0]
+  mag = [0, 0, 0]
+  gyro = [0, 0, 0]
+  
+  def __init__(self, lidar, humidity, steering_angle, temperature, speed, altimeter, acc, mag, gyro):
+    self.lidar = lidar
+    self.humidity = humidity
+    self.steering_angle = steering_angle
+    self.temperature = temperature
+    self.speed = speed
+    self.altimeter = altimeter
+    self.acc = acc
+    self.mag = mag
+    self.gyro = gyro
+    
+   
+  
+  
+  
