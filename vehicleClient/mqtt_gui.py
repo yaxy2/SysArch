0de080e490422e8f8078a4d953fcf08ca571ebb7
Lian <li851fuc@htwg-konstanz.de>
2020-07-21 16:27:40 +0200

    mqtt_gui.py aktualisiert

--- vehicleClient/mqtt_gui.py
+++ vehicleClient/mqtt_gui.py
@@ -10,40 +10,55 @@ temp = 30
 st_angle = 12
 alt = 520
 
-# initialize window
-window = Tk()
-window.title("MQTT Broker")
-window.geometry("680x300")
-window.configure(background="white")
+def initGUI():
+    # initialize window
+    global window
+    window = Tk()
+    window.title("MQTT Broker")
+    window.geometry("680x300")
+    window.configure(background="white")
 
-#################
-# define frames
-#################
+    #################
+    # define frames
+    #################
 
-# left, middle, center
-left_frame = Frame(window, width=200, height=100, highlightbackground="black", highlightcolor="black", highlightthickness=10)
-middle_frame = Frame(window, width=100, height=100, padx=10, pady=10)
-right_frame = Frame(window, width=300, height=100, padx=10, pady=10)
-bottom_frame = Frame(window, width=50, height=50, padx=10, pady=10)
+    # left, middle, center
+    left_frame = Frame(window, width=200, height=100, highlightbackground="black", highlightcolor="black", highlightthickness=10)
+    middle_frame = Frame(window, width=100, height=100, padx=10, pady=10)
+    right_frame = Frame(window, width=300, height=100, padx=10, pady=10)
+    bottom_frame = Frame(window, width=50, height=50, padx=10, pady=10)
 
-# define frame structure and position (grid)
-left_frame.grid(row=0, column=0, sticky="nsew")
-middle_frame.grid(row=0, column=1, sticky="nsew")
-right_frame.grid(row=0, column=2, sticky="nsew")
-bottom_frame.grid(row=0, column=3, columnspan="2", sticky="nsew")
+    # define frame structure and position (grid)
+    left_frame.grid(row=0, column=0, sticky="nsew")
+    middle_frame.grid(row=0, column=1, sticky="nsew")
+    right_frame.grid(row=0, column=2, sticky="nsew")
+    bottom_frame.grid(row=0, column=3, columnspan="2", sticky="nsew")
 
-# define left text frame
-display = Text(left_frame, height="20", width="40", font=("Roboto", 10), fg="white", bg="black", )
-display.pack(side="top", anchor="nw", fill="y", expand=1)
+    # define left text frame
+    global display
+    display = Text(left_frame, height="20", width="40", font=("Roboto", 10), fg="white", bg="black", )
+    display.pack(side="top", anchor="nw", fill="y", expand=1)
 
-# define text frame with sensor data
-display_data = Text(right_frame, height="6", width="15", font=("Roboto", 12), spacing3=6)
-display_data.pack(side="top", anchor="nw", padx=5, pady=5)
-display_data.insert(END, "\n")
+    # define text frame with sensor data
+    global display_data
+    display_data = Text(right_frame, height="6", width="15", font=("Roboto", 12), spacing3=6)
+    display_data.pack(side="top", anchor="nw", padx=5, pady=5)
+    display_data.insert(END, "\n")
 
-# define behaviour is window is streched
-window.grid_rowconfigure(0, weight=1)
-window.grid_columnconfigure(1, weight=1)
+    # define behaviour is window is streched
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_columnconfigure(1, weight=1)
+
+    # draw static labels
+    Label(middle_frame, text="CURRENT VALUES", font=("Roboto", 15)).pack(side="top", anchor="w")
+    drawSensorLabel(middle_frame, "Speed: ")
+    drawSensorLabel(middle_frame, "Temperature: ")
+    drawSensorLabel(middle_frame, "Steering Angle: ")
+    drawSensorLabel(middle_frame, "Altitude: ")
+
+    # define logout button
+    logout_button = Button(bottom_frame, text="Logout", command=userLogout)
+    logout_button.pack(side="bottom", anchor="se")
 
 # function for drawing labels
 def drawSensorLabel(frame, param):
@@ -60,7 +75,7 @@ def userLogin(full_name, user_name, token_id):
 
 def userLogout():
     display.delete(1.0, END)
-    display.insert(END, "Bye bye!\n")
+    display.insert(END, "Bye Bye!\n")
 
 # loop for updating dynamic labels
 def updateData(speed, temp, st_angle, alt):
@@ -71,16 +86,8 @@ def updateData(speed, temp, st_angle, alt):
     display_data.insert(END, str(st_angle) + "°\n")
     display_data.insert(END, str(alt) + " m\n")
 
-# draw static labels
-Label(middle_frame, text="CURRENT VALUES", font=("Roboto", 15)).pack(side="top", anchor="w")
-drawSensorLabel(middle_frame, "Speed: ")
-drawSensorLabel(middle_frame, "Temperature: ")
-drawSensorLabel(middle_frame, "Steering Angle: ")
-drawSensorLabel(middle_frame, "Altitude: ")
-
-# define logout button
-logout_button = Button(bottom_frame, text="Logout", command=userLogout)
-logout_button.pack(side="bottom", anchor="se")
+#initialize GUI
+initGUI()
 
 # call userLogin on user login event
 userLogin(full_name, user_name, token_id)
