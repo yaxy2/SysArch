ce34b2841299363d1a4de4adefa7e5eef54f64b9
yaxy2 <58487644+yaxy2@users.noreply.github.com>
2020-07-21 12:18:18 +0200

    first scelet

--- vehicleClient/rfid_response_client.py
+++ vehicleClient/rfid_response_client.py
@@ -1,1 +1,38 @@
+import paho.mqtt.client as mqtt
+from time import sleep
 
+ds = DataStructRFID_Response()
+jsondump = JSONDumper()
+
+client = mqtt.Client("Pi10AsARFIDResponse")
+
+username = "V1"
+psw = "DE1"
+adress = "localhost"
+port = 8884
+
+def on_connect(client, userdata, flags, rc):
+  if(rc==0):
+    print("CONNACK received with code %d." % (rc))
+  else:
+    print("no Connection")
+  
+def on_message(client, userdata, msg):
+  print("Got Response\n")
+  s = msg.payload
+  
+  if(s["login"]==True):
+    print("%s : Login granted for %s with Username %s. Logged in with RFID Token %s" 
+        % (s["user"]["email"],s["user"]["fullName"],s["user"]["userName"],s["tokenID"]))
+    
+    sleep(0.2)
+  
+  
+client.username_pw_set(username, password=psw)
+client.connect(adress, port)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.subscribe("/SysArch/V1/com2/car")
+client.loop_forever()
